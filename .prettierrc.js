198dea3cd197e5f087a053881e729c446171d77d
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-06-05 13:04:28 -0600

    estructura del proyecto

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,25 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+    /* arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    useTabs: true,
+    tabWidth: 2, */
+
+    "singleQuote": true,
+    "printWidth": 80,
+    "editor.formatOnSave": true,
+    "proseWrap": "always",
+    "tabWidth": 4,
+    "requireConfig": false,
+    "useTabs": false,
+    "trailingComma": "all",
+    "bracketSpacing": true,
+    "jsxBracketSameLine": false,
+    "bracketSameLine": false,
+    "semi": true,
+    "htmlWhitespaceSensitivity": "ignore",
+    "singleAttributePerLine": true,
+    "endOfLine": "auto"
 };
