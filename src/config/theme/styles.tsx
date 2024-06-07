5c0b4302e8eb84d814ee7386ebe28ba666fb1d20
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2024-06-07 11:47:29 -0600

    Consumir nuestro store

---
+++ src/config/theme/styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { create } from 'zustand';
+
+
+export const globalStyles = StyleSheet.create({
+    btnPrimary: {
+        backgroundColor: 'black',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius:100,
+        margin: 10,
+    }
+})
--- src/presentation/navigation/StackNavigator.tsx
+++ src/presentation/navigation/StackNavigator.tsx
@@ -15,7 +15,7 @@ const Stack = createStackNavigator<RootStackParams>();
 export const StackNavigator = () => {
     return (
         <Stack.Navigator
-            initialRouteName='LoadingScreen'
+            initialRouteName='PermissionsScreen'
             screenOptions={{
                 headerShown: false,
                 cardStyle:{
